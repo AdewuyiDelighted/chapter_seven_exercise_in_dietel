ef619e7a43f98d79c58ae9dcf7c0bbd5533eff4c
delighted <deborahdelighted5@gmail.com>
2023-11-30 22:21:53 +0100

    lol

---
+++ src/DeckOfCardTest.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+public class DeckOfCardTest {
+    public static void main(String[] args) {
+        DeckOfCards myDeckOfCard = new DeckOfCards();
+        myDeckOfCard.shuffle();
+
+    }
+}
